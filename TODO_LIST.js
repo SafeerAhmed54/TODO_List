805c874109d2b6acc12d87a0efdd2ee4e3b368f5
Safeer Ahmed <pakistanireacts9@gmail.com>
2024-11-23 23:20:28 +0500

    Added TODO Task
    Added Through while loop
    Added with bool while loop

---
+++ TODO_LIST.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+let taskList = [];
+let taskList2 = [];
+let condtion = true;
+const answer = await inquirer.prompt([
+    {
+        message: "Hello Please Enter the no Task",
+        type: "number",
+        name: "NoOfTasks"
+    }
+]);
+let i = 0;
+while (i < answer.NoOfTasks) {
+    const answer = await inquirer.prompt([
+        {
+            message: "Hello Please Enter the Task",
+            type: "input",
+            name: "taskName"
+        }
+    ]);
+    taskList[i] = answer.taskName;
+    i++;
+}
+let j = 0;
+while (j < taskList.length) {
+    console.log(`Your Task No ${j + 1} is ${taskList[j]}`);
+    j++;
+}
+while (condtion) {
+    const answer2 = await inquirer.prompt([
+        {
+            message: "Hello Please Enter the Task",
+            type: "input",
+            name: "taskName"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to Continue",
+            default: false
+        }
+    ]);
+    taskList2.push(answer2.addMore);
+    condtion = answer2.addMore;
+}
